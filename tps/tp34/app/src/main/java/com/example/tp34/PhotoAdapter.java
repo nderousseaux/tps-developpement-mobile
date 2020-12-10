2979eed81dba63219aa00c53385ac26ad6c7770f
Nathanaël Derousseaux <n.derousseaux@icloud.com>
2020-12-10 18:00:07 +0100

    Correction faute de frappe

--- tps/tp34/app/src/main/java/com/example/tp34/PhotoAdapter.java
+++ tps/tp34/app/src/main/java/com/example/tp34/PhotoAdapter.java
@@ -14,7 +14,7 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.squareup.picasso.c;
+import com.squareup.picasso.Picasso;
 
 
 import java.util.List;
